6112c5d10979915cad9556126b51e4f9dae276b9
Stefan Froemken <froemken@gmail.com>
2018-11-20 17:29:09 +0100

    Move all TCA modifying methods into Overrides

---
+++ Classes/TCA/Overrides/tt_content.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die ('Access denied.');
+}
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'JWeiland.socialservices',
+    'Socialservices',
+    'LLL:EXT:socialservices/Resources/Private/Language/locallang_db.xlf:plugin.title'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -3,16 +3,14 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'JWeiland.socialservices',
-    'Socialservices',
-    'LLL:EXT:socialservices/Resources/Private/Language/locallang_db.xlf:plugin.title'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_socialservices_domain_model_helpdesk',
+    'EXT:socialservices/Resources/Private/Language/locallang_csh_tx_socialservices_domain_model_helpdesk.xlf'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_socialservices_domain_model_district',
+    'EXT:socialservices/Resources/Private/Language/locallang_csh_tx_socialservices_domain_model_district.xlf'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('socialservices', 'Configuration/TypoScript', 'Social Services');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_socialservices_domain_model_helpdesk', 'EXT:socialservices/Resources/Private/Language/locallang_csh_tx_socialservices_domain_model_helpdesk.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_socialservices_domain_model_helpdesk');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_socialservices_domain_model_district', 'EXT:socialservices/Resources/Private/Language/locallang_csh_tx_socialservices_domain_model_district.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_socialservices_domain_model_district');
