442935c05fc0f0f455a0c4254b56c58296086d80
Stefan Froemken <froemken@gmail.com>
2018-11-20 14:33:57 +0100

    Make use of Maps2 Registry

--- Configuration/TCA/Overrides/tx_socialservices_domain_model_helpdesk.php
+++ Configuration/TCA/Overrides/tx_socialservices_domain_model_helpdesk.php
@@ -8,3 +8,25 @@
         'exclude' => 1
     ]
 );
+
+// Add tx_maps2_uid column to helpdesk table
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('maps2')) {
+    \JWeiland\Maps2\Tca\Maps2Registry::getInstance()->add(
+        'socialservices',
+        'tx_socialservices_domain_model_helpdesk',
+        [
+            'addressColumns' => ['street', 'house_number', 'zip', 'city'],
+            'defaultCountry' => 'Deutschland',
+            'defaultStoragePid' => [
+                'extKey' => 'socialservices',
+                'property' => 'poiCollectionPid'
+            ],
+            'synchronizeColumns' => [
+                [
+                    'foreignColumnName' => 'title',
+                    'poiCollectionColumnName' => 'title'
+                ]
+            ]
+        ]
+    );
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,16 +4,9 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'JWeiland.' . $_EXTKEY,
+    'JWeiland.socialservices',
     'Socialservices',
     ['Helpdesk' => 'list, show, search'],
     // non-cacheable actions
     ['Helpdesk' => 'search']
 );
-
-// use hook to automatically add a map record to current yellow page
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \JWeiland\Socialservices\Tca\CreateMap::class;
-
-$extConf = unserialize($_EXTCONF);
-$tsConfig = 'ext.socialservices.pid = ' . (int)$extConf['poiCollectionPid'];
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($tsConfig);
