adc202438791527d872c563552d29626b754cf01
Stefan Froemken <froemken@gmail.com>
2022-05-10 12:20:30 +0200

    Add events for Controller Actions

---
+++ Classes/EventListener/AddGlossaryEventListener.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/socialservices.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Socialservices\EventListener;
+
+use JWeiland\Glossary2\Service\GlossaryService;
+use JWeiland\Socialservices\Domain\Repository\HelpdeskRepository;
+use JWeiland\Socialservices\Event\PostProcessFluidVariablesEvent;
+use TYPO3\CMS\Core\Utility\ArrayUtility;
+
+class AddGlossaryEventListener extends AbstractControllerEventListener
+{
+    /**
+     * @var GlossaryService
+     */
+    protected $glossaryService;
+
+    /**
+     * @var HelpdeskRepository
+     */
+    protected $helpdeskRepository;
+
+    protected $allowedControllerActions = [
+        'Helpdesk' => [
+            'list'
+        ]
+    ];
+
+    public function __construct(GlossaryService $glossaryService, HelpdeskRepository $helpdeskRepository)
+    {
+        $this->glossaryService = $glossaryService;
+        $this->helpdeskRepository = $helpdeskRepository;
+    }
+
+    public function __invoke(PostProcessFluidVariablesEvent $event): void
+    {
+        if ($this->isValidRequest($event)) {
+            $event->addFluidVariable(
+                'glossar',
+                $this->glossaryService->buildGlossary(
+                    $this->helpdeskRepository->getQueryBuilderToFindAllEntries(),
+                    $this->getOptions($event)
+                )
+            );
+        }
+    }
+
+    protected function getOptions(PostProcessFluidVariablesEvent $event): array
+    {
+        $options = [
+            'extensionName' => 'socialservices',
+            'pluginName' => 'socialservices',
+            'controllerName' => 'Helpdesk',
+            'column' => 'title',
+            'settings' => $event->getSettings()
+        ];
+
+        if (
+            isset($event->getSettings()['glossary'])
+            && is_array($event->getSettings()['glossary'])
+        ) {
+            ArrayUtility::mergeRecursiveWithOverrule($options, $event->getSettings()['glossary']);
+        }
+
+        return $options;
+    }
+}
---
+++ Classes/EventListener/AddPaginatorEventListener.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/socialservices.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Socialservices\EventListener;
+
+use JWeiland\Socialservices\Event\PostProcessFluidVariablesEvent;
+use JWeiland\Socialservices\Pagination\RequestPagination;
+use TYPO3\CMS\Core\Utility\MathUtility;
+use TYPO3\CMS\Extbase\Pagination\QueryResultPaginator;
+
+class AddPaginatorEventListener extends AbstractControllerEventListener
+{
+    /**
+     * @var int
+     */
+    protected $itemsPerPage = 15;
+
+    protected $allowedControllerActions = [
+        'Helpdesk' => [
+            'list',
+            'search'
+        ]
+    ];
+
+    public function __invoke(PostProcessFluidVariablesEvent $event): void
+    {
+        if ($this->isValidRequest($event)) {
+            $paginator = new QueryResultPaginator(
+                $event->getFluidVariables()['helpdesks'],
+                $this->getCurrentPage($event),
+                $this->getItemsPerPage($event)
+            );
+
+            $event->addFluidVariable('actionName', $event->getActionName());
+            $event->addFluidVariable('paginator', $paginator);
+            $event->addFluidVariable('helpdesks', $paginator->getPaginatedItems());
+            $event->addFluidVariable('pagination', new RequestPagination($paginator));
+        }
+    }
+
+    protected function getCurrentPage(PostProcessFluidVariablesEvent $event): int
+    {
+        $currentPage = 1;
+        if ($event->getRequest()->hasArgument('currentPage')) {
+            // $currentPage have to be positive and greater than 0
+            // See: AbstractPaginator::setCurrentPageNumber()
+            $currentPage = MathUtility::forceIntegerInRange(
+                (int)$event->getRequest()->getArgument('currentPage'),
+                1
+            );
+        }
+
+        return $currentPage;
+    }
+
+    protected function getItemsPerPage(PostProcessFluidVariablesEvent $event): int
+    {
+        $itemsPerPage = $this->itemsPerPage;
+        if (isset($event->getSettings()['pageBrowser']['itemsPerPage'])) {
+            $itemsPerPage = $event->getSettings()['pageBrowser']['itemsPerPage'];
+        }
+        return (int)$itemsPerPage;
+    }
+}
